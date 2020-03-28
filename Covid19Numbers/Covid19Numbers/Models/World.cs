74f6bb85e9324cc33c35d0daa4e601363b2d04d3
cumbie <dougec@gmail.com>
2020-03-28 11:20:03 -0400

    main page shows global stats

---
+++ Covid19Numbers/Covid19Numbers/Models/World.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Covid19Numbers.Models
+{
+    public class World
+    {
+        public World()
+        {
+        }
+
+        [JsonProperty("cases")]
+        public int Cases { get; set; }
+
+        [JsonProperty("deaths")]
+        public int Deaths { get; set; }
+
+        [JsonProperty("recovered")]
+        public int Recovered { get; set; }
+
+        [JsonProperty("updated")]
+        public long Updated { get; set; }
+
+        [JsonProperty("active")]
+        public int Active { get; set; }
+
+        public DateTime UpdateTime => DateTimeOffset.FromUnixTimeMilliseconds(this.Updated).DateTime;
+    }
+}
--- Covid19Numbers/Covid19Numbers/ViewModels/MainPageViewModel.cs
+++ Covid19Numbers/Covid19Numbers/ViewModels/MainPageViewModel.cs
@@ -17,13 +17,14 @@ namespace Covid19Numbers.ViewModels
     public class MainPageViewModel : ViewModelBase
     {
         bool _running = false;
-        int _counterStartDelay = 3000;
+        int _counterStartDelay = 1000;
         int _counterDelay = 1000;
 
         HttpClient _client;
 
         string _country = "US";
 
+        string _worldUrl = "https://corona.lmao.ninja/all";
         string _countryUrl = "https://corona.lmao.ninja/countries/";
         string _statesUrl = "https://corona.lmao.ninja/states";
 
@@ -39,6 +40,8 @@ namespace Covid19Numbers.ViewModels
             Task.Factory.StartNew(CounterThread);
         }
 
+        #region Properties
+
         private int _totalCases = 0;
         public int TotalCases
         {
@@ -46,12 +49,21 @@ namespace Covid19Numbers.ViewModels
             set { SetProperty(ref _totalCases, value); }
         }
 
-        private int _todayCases = 0;
-        public int TodayCases
+        private int _totalDeaths = 0;
+        public int TotalDeaths
         {
-            get { return _todayCases; }
-            set { SetProperty(ref _todayCases, value); }
+            get { return _totalDeaths; }
+            set { SetProperty(ref _totalDeaths, value); }
         }
+
+        private DateTime _worldLastUpdate;
+        public DateTime WorldLastUpdate
+        {
+            get { return _worldLastUpdate; }
+            set { SetProperty(ref _worldLastUpdate, value); }
+        }
+
+        #endregion
 
         int _stateCnt = 0;
         void CounterThread()
@@ -63,16 +75,22 @@ namespace Covid19Numbers.ViewModels
             {
                 while (_running)
                 {
-                    var countryResponse = _client.GetAsync($"{_countryUrl}{_country}").GetAwaiter().GetResult();
-                    var countryJson = countryResponse.Content.ReadAsStringAsync().GetAwaiter().GetResult();
-                    var country = ParseCountry(countryJson);
+                    //var countryResponse = _client.GetAsync($"{_countryUrl}{_country}").GetAwaiter().GetResult();
+                    //var countryJson = countryResponse.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                    //var country = ParseCountry(countryJson);
+
+                    var worldResponse = _client.GetAsync(_worldUrl).GetAwaiter().GetResult();
+                    var worldson = "{\"cases\":597458,\"deaths\":27370,\"recovered\":133373,\"updated\":1585375470343,\"active\":436715}";
+                    //var worldson = worldResponse.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                    var world = JsonConvert.DeserializeObject<World>(worldson);
 
                     Device.BeginInvokeOnMainThread(() =>
                     {
-                        this.TotalCases = country.Cases;
-                        this.TodayCases = country.TodayCases;
+                        this.TotalCases = world.Cases;
+                        this.TotalDeaths = world.Deaths;
+                        this.WorldLastUpdate = world.UpdateTime;
                     });
-
+                    
                     //if (_stateCnt++ == 5)
                     //{
                     //    var stateResponse = _client.GetAsync(_statesUrl).GetAwaiter().GetResult();
@@ -88,9 +106,5 @@ namespace Covid19Numbers.ViewModels
             }
         }
 
-        private Country ParseCountry(string json)
-        {
-            return JsonConvert.DeserializeObject<Country>(json);
-        }
     }
 }
